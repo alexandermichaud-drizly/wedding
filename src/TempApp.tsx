2e30b12091bd0650d44ddd33d7fa77b37e2c7ae9
ARM <alexander.michaud@aya.yale.edu>
2022-01-27 12:19:12 -0500

    Styling on temp page

--- src/TempApp.tsx
+++ src/TempApp.tsx
@@ -1,15 +1,19 @@
 import { Dialog, DialogTitle } from '@mui/material';
 import ring from "./assets/ring.jpg";
+import s from "./TempApp.module.scss";
 
 const TempApp = (): JSX.Element => {  
   return (
-    <div>
-      <img src={ring} alt="ring" />
-        <Dialog open>
-          <DialogTitle>
-            Check back here for our full wedding website on March 27, 2022!
-          </DialogTitle>
-        </Dialog>
+    <div className={s.Content}>
+      <div className={s.BackgroundImage} />
+      <div className={s.Ring} > 
+        <img src={ring} alt="ring" />
+      </div>
+      <Dialog open>
+        <DialogTitle>
+          Check back here on March 27 for our full wedding website!
+        </DialogTitle>
+      </Dialog>
     </div>
   )
 };
