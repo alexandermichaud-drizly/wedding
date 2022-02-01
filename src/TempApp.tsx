3f238ed8737488b01bcfc1ee18fc2675e5da3b91
ARM <alexander.michaud@aya.yale.edu>
2022-02-01 16:57:19 -0500

    Styling for modal

--- src/TempApp.tsx
+++ src/TempApp.tsx
@@ -1,4 +1,5 @@
-import { Dialog, DialogTitle } from '@mui/material';
+import { Dialog, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import cn from 'classnames';
 import ring from "./assets/ring.jpg";
 import s from "./TempApp.module.scss";
 
@@ -9,10 +10,15 @@ const TempApp = (): JSX.Element => {
       <div className={s.Ring} > 
         <img src={ring} alt="ring" />
       </div>
-      <Dialog open>
-        <DialogTitle classes={{ root: s.DialogTitle }}>
-          Check back here on March 27 for our full wedding website!
+      <Dialog open classes={{ paper: cn(s.Paper, s.Dialog) }}>
+        <DialogTitle className={cn(s.TitleText, s.Dialog)} >
+          Coming soon...
         </DialogTitle>
+        <DialogContent>
+          <DialogContentText className={cn(s.ContentText, s.Dialog)}>
+            Guests will be notified when our wedding site is ready
+          </DialogContentText>
+        </DialogContent>
       </Dialog>
     </div>
   )
