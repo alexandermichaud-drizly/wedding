41b702c428606c5d01151737c0b9372e4ab1b461
ARM <alexander.michaud@aya.yale.edu>
2022-04-17 16:15:15 -0400

    Use real app

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import LandingPage from './pages/landing';
 
-const useRealApp = false;
 
 ReactDOM.render(
   <React.StrictMode>
-    {useRealApp ? <App /> : <LandingPage tempPage />}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
