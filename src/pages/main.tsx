cd024763ef20fa58bf2c765cd93da4d38e5d1074
ARM <alexander.michaud@aya.yale.edu>
2022-05-03 11:14:42 -0400

    Castle image. Next explore getting rid of heading

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,33 +1,31 @@
 import { useEffect, useRef, useState, memo } from 'react';
 import s from '../styles/main.module.scss';
 import valley from '../assets/valley.mp4';
+import castle from '../assets/castle.jpg';
 import { Fade } from '@mui/material';
 
-const VIDEO_TRANSITION_SECONDS = 5;
-const VIDEO_TRANSITION = VIDEO_TRANSITION_SECONDS * 1000;
-const VIDEO_TRANSITION_ABBREVIATED = 2500;
-const VIDEO_TRANSITION_EXTENDED = 6500;
 
-const Heading = memo((props: { showHeading: boolean, endVideoEarly: boolean }): JSX.Element => {
-  return (
-    <Fade in={props.showHeading} timeout={props.endVideoEarly ? VIDEO_TRANSITION_ABBREVIATED : VIDEO_TRANSITION_EXTENDED}>
-      <h1 style={{ textAlign: 'center' }}>Join Us on May 23</h1>
-    </Fade>
-  )
-});
+const FADE_IN_VIDEO_SECONDS = 5;
+const FADE_IN_MAIN_SECONDS = 6;
+const FADE_IN_MAIN_MS = FADE_IN_MAIN_SECONDS * 1000;
+const FADE_IN_VIDEO_MS = FADE_IN_VIDEO_SECONDS * 1000;
+const FADE_IN_MAIN_ABBREVIATED_MS = 2500;
+const FADE_IN_HEADER_MS = 6500;
+const FADE_IN_CASTLE_MS = 10000;
+const FADE_IN_CASTLE_ABBREVIATED_MS = 5000;
+const PLAYBACK_RATE = 0.7;
 
 const Main = (): JSX.Element => {
 
   const videoRef = useRef() as React.RefObject<HTMLVideoElement>;
 
-  const [fadeOut, setFadeOut] = useState(false);
-  const [videoComplete, setVideoComplete] = useState(false);
+  const [fadeInMain, setFadeInMain] = useState(false);
   const [showHeading, setShowHeading] = useState(false);
   const [endVideoEarly, setEndVideoEarly] = useState(false);
 
   useEffect((): void => {
     const video = videoRef.current;
-    if (video) video.playbackRate = 0.7;
+    if (video) video.playbackRate = PLAYBACK_RATE;
   });
 
   const handleTimeUpdate = (): void => {
@@ -35,22 +33,21 @@ const Main = (): JSX.Element => {
     if (video) {
       const { currentTime, duration } = video;
       if (!showHeading && currentTime > duration / 3) setShowHeading(true);
-      if (!fadeOut && currentTime > duration - VIDEO_TRANSITION_SECONDS) setFadeOut(true);
-      if (!videoComplete && currentTime === duration) setVideoComplete(true);
+      if (!fadeInMain && currentTime > duration - FADE_IN_MAIN_SECONDS) setFadeInMain(true);
     }
   }
 
   const onClickVideo = (): void => {
-    if (!fadeOut && !videoComplete && !endVideoEarly) { 
-      setFadeOut(true); 
+    if (!fadeInMain && !endVideoEarly) { 
+      setFadeInMain(true); 
       setShowHeading(true); 
       setEndVideoEarly(true);
     }
   };
 
   return (
-    <>
-      <Fade in={!fadeOut} timeout={{ enter: VIDEO_TRANSITION, exit: endVideoEarly ? VIDEO_TRANSITION : VIDEO_TRANSITION_ABBREVIATED }} unmountOnExit>
+    <div className={s.Wrapper}>
+      <Fade in={!fadeInMain} timeout={{ enter: FADE_IN_VIDEO_MS, exit: endVideoEarly ? FADE_IN_MAIN_ABBREVIATED_MS : FADE_IN_MAIN_MS }} unmountOnExit>
         <video 
           autoPlay 
           muted id="valley" 
@@ -62,8 +59,11 @@ const Main = (): JSX.Element => {
           <source src={valley} type="video/mp4" />
         </video>
       </Fade>
-      <Heading showHeading={showHeading} endVideoEarly={endVideoEarly}/>
-    </>
+      <Fade in={showHeading} timeout={endVideoEarly ? FADE_IN_MAIN_ABBREVIATED_MS : FADE_IN_HEADER_MS}>
+        <h1 style={{ textAlign: 'center', color: "white" }}>Join Us on May 23</h1>
+      </Fade>
+      <Fade in={fadeInMain} timeout={endVideoEarly ? FADE_IN_CASTLE_ABBREVIATED_MS : FADE_IN_CASTLE_MS }><div className={s.Castle} /></Fade>
+    </div>
   );
 };
 
