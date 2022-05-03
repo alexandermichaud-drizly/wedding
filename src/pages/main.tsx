b93ece28277c53a8a9443063e7e564a1a483520c
ARM <alexander.michaud@aya.yale.edu>
2022-05-03 18:47:33 -0400

    Still tweaking heading fade

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -72,8 +72,8 @@ const Main = (): JSX.Element => {
           <source src={valley} type="video/mp4" />
         </video>
       </Fade>
-      <Fade in={showHeading} timeout={endVideoEarly ? FADE_IN_MAIN_ABBREVIATED_MS : FADE_IN_HEADER_MS} mountOnEnter>
-        <h1 style={{ textAlign: 'center', marginTop: '48px' }}>May 23, 2023</h1>
+      <Fade in={showHeading && fadeInVideo} timeout={endVideoEarly ? FADE_IN_MAIN_ABBREVIATED_MS : FADE_IN_HEADER_MS} mountOnEnter>
+        <h1 className={s.Heading}>Join Us In Tuscany, May 2023</h1>
       </Fade>
       <Fade in={fadeInImage} timeout={endVideoEarly ? FADE_IN_MAIN_ABBREVIATED_MS : FADE_IN_CASTLE_MS } easing="ease-in" mountOnEnter>
         <img className={s.Castle} src={castle} alt="castle"/>
