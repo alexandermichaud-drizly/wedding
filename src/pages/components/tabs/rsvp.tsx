1efc40911123882783f96a4c6f6bf6c29bb79413
ARM <alexander.michaud@aya.yale.edu>
2022-06-05 19:10:22 -0400

    Placeholder pages

--- src/pages/components/tabs/rsvp.tsx
+++ src/pages/components/tabs/rsvp.tsx
@@ -1,3 +1,10 @@
-const RSVP = (): JSX.Element => <></>;
+import s from '../../../styles/tabbed_content.module.scss';
+
+const placeholder = "Guests will be able to RSVP through the site later this summer. We'll let you know when it's ready!"
+
+const RSVP = (): JSX.Element => 
+  <div className={s.RSVP}>
+    {placeholder}
+  </div>;
 
 export default RSVP;
--- src/pages/components/tabs/special_activities.tsx
+++ src/pages/components/tabs/special_activities.tsx
@@ -1,8 +1,12 @@
 import s from '../../../styles/tabbed_content.module.scss';
 
+const placeholder = "The second day of the festivities will afford guests time for special activities and excursions, e.g. horseback riding, Italian cooking classes, etc. We'll let you know when you can book spots!"
+
 const SpecialActivities = (): JSX.Element => 
   <div className={s.SpecialActivities}>
-
+    <div className={s.RSVP}>
+      {placeholder}
+    </div>;
   </div>;
 
 export default SpecialActivities;
