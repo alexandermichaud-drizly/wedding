68ffd5fbcdea0421eb94739450b59a1a1be654ca
ARM <alexander.michaud@aya.yale.edu>
2022-06-05 04:35:44 -0400

    Attire styling

--- src/pages/components/tabs/attire.tsx
+++ src/pages/components/tabs/attire.tsx
@@ -1,4 +1,5 @@
 import s from '../../../styles/tabbed_content.module.scss';
+import cn from 'classnames';
 
 type Occasion = {
   title: string;
@@ -10,20 +11,32 @@ type Occasion = {
 const occasions: Occasion[] = [
   {
     title: "Welcome Dinner",
-    general: "Summer Cocktail Attire",
+    general: "White Night",
     men: "Jackets, Slacks / Chinos. Ties optional.",
     women: "Cocktail Dresses or Sun Dresses."
+  },
+  {
+    title: "Throughout the Week",
+    general: "Comfortable",
+    men: "Bathing suits welcome. Summer wear recommended.",
+    women: "Bathing suits welcome. Summer wear recommended."
+  }, 
+  {
+    title: "Wedding Dinner",
+    general: "Black Tie Optional",
+    men: "Suit and Tie. Tuxedo optional.",
+    women: "Cocktail Dresses or Long Dresses."
   }
 ];
 
 const Attire = (): JSX.Element => 
   <div className={s.Attire}>
     {occasions.map(occasion => 
-      <div>
-        <h1>{occasion.title}</h1>
-        <div>{occasion.general}</div>
-        <div>{`Gentlemen: ${occasion.men}`}</div>
-        <div>{`Ladies: ${occasion.women}`}</div>
+      <div className={cn(s.Occasion, s.Container)}>
+        <h1 className={cn(s.Occasion, s.Title)}>{occasion.title}</h1>
+        <div className={cn(s.Occasion, s.Subtitle)}>{occasion.general}</div>
+        <div><span className={s.Gender}>Gentlemen: </span>{occasion.men}</div>
+        <div><span className={s.Gender}>Ladies: </span>{occasion.women}</div>
       </div>
     )}
   </div>;
