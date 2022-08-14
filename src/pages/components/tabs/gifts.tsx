026965ea3fc1907c1b1b9fab4d32fb40bc687a80
ARM <alexander.michaud@aya.yale.edu>
2022-08-13 23:36:05 -0400

    Update Gifts page

--- src/pages/components/tabs/gifts.tsx
+++ src/pages/components/tabs/gifts.tsx
@@ -1,18 +1,19 @@
-import * as React from "react";
-import s from "../../../styles/main.module.scss";
-
-const expectations =
-  "Making the journey to Italy is more than we could ever expect from our guests, so please do not feel obligated to give us anything more. The greatest gift we can receive is your presence on our special day.";
-const request =
-  "We are currently in the process of searching for a home in DC, so if you are strongly inclined to give us something, we would greatly appreciate contributions to our home fund. Anything helps, but again, nothing is expected.";
-const reachOut =
-  "It's most helpful financially if the gift is earmarked for a downpayment, so if you are interested, please reach out to us to coordinate.";
+import * as React from 'react';
+import s from '../../../styles/main.module.scss';
 
 const Gifts = (): JSX.Element => (
   <div className={s.GiftsCopy}>
-    <div>{expectations}</div>
-    <div>{request}</div>
-    <div>{reachOut}</div>
+    <div>
+      Making the journey to Italy is more than we could ever expect from our
+      guests, so please do not feel obligated to give us anything more. The
+      greatest gift we can receive is your presence on our special day.
+    </div>
+    <div>
+      We recently purchased a home in DC, so if you are strongly inclined to
+      give us something, we would greatly appreciate contributions that will go
+      toward furnishings, repairs, and upkeep. Anything helps, but again,
+      nothing is expected.
+    </div>
   </div>
 );
 
