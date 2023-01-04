de9f11e75a06c474b18eb6f370f6d5c2cd70cf34
ARM <alexander.michaud@aya.yale.edu>
2023-01-04 10:22:04 -0500

    More copy changes

--- src/pages/components/tabs/getting_there.tsx
+++ src/pages/components/tabs/getting_there.tsx
@@ -6,20 +6,20 @@ const GettingThere = (): JSX.Element => (
     <h1>Getting There</h1>
     <div>
       Regardless of your travel plans, contact us when you have determined when
-      and how you&apos;ll arrive. We can help you arrange transportation
-      that&apos;s offered by the venue. The most convenient point of entry will
-      be the Florence Airport, Peretola (FLR). You might also consider flying
-      into Pisa (PSA) or Bologna (BLQ), from which we can also arrange a
-      pick-up. In any case, you will almost certainly have to stop over in a
-      larger European city on the way.
+      and how you&apos;ll arrive. We can help you arrange transportation offered
+      by the venue from certain points of entry. The most convenient option
+      would be the Florence Airport, Peretola (FLR), but we can also arrange for
+      you to be picked up from the airports in Pisa (PSA) or Bologna (BLQ). In
+      any case, you will almost certainly have to stop over in a larger European
+      city on the way.
     </div>
     <div>
       Another option would be to fly into Rome (FCO) and take a train to
-      Florence. Many European budget airlines fly to Rome, the trains to
-      Florence are affordable, and the castle can pick you up from Santa Maria
-      Novella station. Trains in Italy are generally affordable, so you might
-      also consider arriving by train if you are traveling in Italy beforehand.
-      If you have rented a car, there is free parking on-site at the venue.
+      Florence. Many budget airlines fly to Rome, the trains to Florence are
+      inexpensive, and the castle can pick you up from Santa Maria Novella
+      station. Trains in Italy are generally affordable, so you might also
+      consider arriving by train if you are traveling in Italy beforehand. If
+      you have rented a car, there is free parking on-site at the venue.
     </div>
     <div>
       The castle is approximately a half hour drive from Florence.
