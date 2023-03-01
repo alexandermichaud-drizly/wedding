6cfd5fb60aba28ee6102f316168b00cb6adc9bc7
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-02-28 19:54:16 -0500

    Update getting there

--- src/pages/components/tabs/getting_there.tsx
+++ src/pages/components/tabs/getting_there.tsx
@@ -8,26 +8,26 @@ const GettingThere = (): JSX.Element => (
       Regardless of your travel plans, contact us when you have determined when
       and how you&apos;ll arrive. We can help you arrange transportation offered
       by the venue from certain points of entry. The most convenient option
-      would be the Florence Airport, Peretola (FLR), but we can also arrange for
-      you to be picked up from the airports in Pisa (PSA) or Bologna (BLQ). In
-      any case, you will almost certainly have to stop over in a larger European
-      city on the way.
+      would be the Florence Airport, Peretola (FLR). We can also arrange for you
+      to be picked up from the airports in Pisa (PSA) or Bologna (BLQ), but the
+      ride will be more expensive.
     </div>
     <div>
       Another option would be to fly into Rome (FCO) and take a train to
       Florence. Many budget airlines fly to Rome, the trains to Florence are
-      inexpensive, and the castle can pick you up from Santa Maria Novella
+      inexpensive, and we can arrange for pickup from Santa Maria Novella
       station. Trains in Italy are generally affordable, so you might also
       consider arriving by train if you are traveling in Italy beforehand. If
-      you have rented a car, there is free parking on-site at the venue.
+      you have rented a car, there is free parking on-site at the venue. The
+      castle is approximately a half hour drive from Florence.
     </div>
     <div>
-      The castle is approximately a half hour drive from Florence.
-      Transportation arranged through us will be able drop you off at the door
-      of your villa. We do not recommend arranging a private taxi or uber to the
-      castle. They will be more expensive and will not be able to take you
-      directly to the villa in which you are staying. Let us save you the
-      trouble of bringing your bags up the hill!
+      Transportation arranged through the venue must be paid in euros, but we
+      will be able to minimize costs to guests by grouping those arriving at
+      similar times from the same point of entry. We do not recommend arranging
+      a private taxi or uber to the castle. They will be more expensive and will
+      not be able to take you directly to the villa in which you are staying.
+      Let us save you the trouble of bringing your bags up the hill!
     </div>
     <div>Address: Via di Santa Brigida, 9, 50065 Pontassieve FI, Italy</div>
   </div>
