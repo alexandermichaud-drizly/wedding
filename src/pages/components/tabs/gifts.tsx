7f59e4d37764cd7514324cf72dd64eab0c8975fd
Alexander Rodion Michaud <alexanderrodionmichaud@Alexanders-MacBook-Pro.local>
2023-04-01 13:29:38 -0400

    Remove codes

--- src/pages/components/tabs/gifts.tsx
+++ src/pages/components/tabs/gifts.tsx
@@ -1,12 +1,4 @@
 import * as React from 'react';
-import s from '../../../styles/main.module.scss';
-
-const venmoSrc =
-  'https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/3e954264-7c4d-4fc6-605a-09280385b200/public';
-const paypalSrc =
-  'https://imagedelivery.net/Xh-8qVol1Pftdals16ieKg/69fbe449-d286-40f3-b3f1-8aca6ec10000/public';
-const venmoLink = 'https://venmo.com/code?user_id=1702589049602048491';
-const paypalLink = 'https://paypal.me/andreaandalexander';
 
 const Gifts = (): JSX.Element => {
   return (
@@ -20,20 +12,7 @@ const Gifts = (): JSX.Element => {
       <div>
         We recently purchased our dream home, so if you are inclined to give us
         something, we would greatly appreciate contributions toward the cost of
-        furnishings, repairs, and upkeep. Click on or scan the QR codes below to
-        send a gift online, or else feel free to get in touch with us directly.
-      </div>
-      <div className={s.QRCodes}>
-        <div className={s.Code}>
-          <a href={paypalLink}>
-            <img className={s.PayPal} src={paypalSrc} alt="PayPal QR Code" />
-          </a>
-        </div>
-        <div className={s.Code}>
-          <a href={venmoLink}>
-            <img className={s.Venmo} src={venmoSrc} alt="Venmo QR Code" />
-          </a>
-        </div>
+        furnishings, repairs, and upkeep.
       </div>
     </div>
   );
